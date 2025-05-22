bb8fb449030f657c6f48e63663e73d2acf4312e5
Nikolai Orlov <nicolasorlov174@yandex.com>
2025-05-22 17:00:49 +0200

    fix: adapted code

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     # Create a test input tensor (batch_size, channels, height, width)
     x = torch.randn(1, 1, 32, 32).to(device)
-    # print(f"Input shape: {x.shape}")
+    print(f"Input shape: {x.shape}")
     # print('Input device: ', x.get_device())
     # print('Model device: ', next(model.parameters()).get_device())
     # print('ModelV1 device: ', next(modelV1.parameters()).get_device())
@@ -22,13 +22,12 @@ if __name__ == "__main__":
     # Pass the input through the model
     output = modelV1(x)
     print(f"Output shape: {output.shape}")
-    print('----')
     # output = model(x)
     # print(f"Output shape old: {output.shape}")
 
-    # # Print model statistics
-    # total_params = sum(p.numel() for p in modelV1.parameters())
-    # print(f"\nTotal parameters: {total_params:,}")
+    # Print model statistics
+    total_params = sum(p.numel() for p in modelV1.parameters())
+    print(f"\nTotal parameters: {total_params:,}")
 
     # trainable_params = sum(p.numel()
     #                        for p in modelV1.parameters() if p.requires_grad)
