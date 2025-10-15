55ecf86c445789e168f4119c6fd0a48799100c2c
Nikolai Orlov <nicolasorlov174@yandex.com>
2025-10-15 19:49:47 +0200

    style: fixed imports

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,31 +2,32 @@ import torch
 
 from src.models.unet_skip_v0 import UNet
 from src.models.unet_skip_v1 import UNet as UNetV1
-
+from src.models.unet_skip_v2 import UNet as UNetV2
 
 # Testing the model
 if __name__ == "__main__":
     # Create a UNet model
     device = 'cpu'
-    # model = UNet(n_channels=[1], o_channels=[1]).to(device)
+    model = UNet(n_channels=[1], o_channels=[1]).to(device)
     modelV1 = UNetV1(n_channels=[1], o_channels=[1]).to(device)
+    modelV2 = UNetV2(n_channels=[1], o_channels=[1]).to(device)
     # print('Model: ', modelV1)
 
     # Create a test input tensor (batch_size, channels, height, width)
-    x = torch.randn(1, 1, 128, 128).to(device)
+    x = torch.randn(1, 1, 32, 32).to(device)
     print(f"Input shape: {x.shape}")
     # print('Input device: ', x.get_device())
     # print('Model device: ', next(model.parameters()).get_device())
     # print('ModelV1 device: ', next(modelV1.parameters()).get_device())
 
     # Pass the input through the model
-    output = modelV1(x)
+    output = modelV2(x)
     print(f"Output shape: {output.shape}")
     # output = model(x)
     # print(f"Output shape old: {output.shape}")
 
     # Print model statistics
-    total_params = sum(p.numel() for p in modelV1.parameters())
+    total_params = sum(p.numel() for p in modelV2.parameters())
     print(f"\nTotal parameters: {total_params:,}")
 
     # trainable_params = sum(p.numel()
