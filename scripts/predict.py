c44bfcc3d76e938c15902ca3204fcf9a1f05692e
Nikolai Orlov <nicolasorlov174@yandex.com>
2025-10-20 10:22:30 +0200

    fix: use image the same size as in training; ssim mitric for prediction

--- scripts/predict.py
+++ scripts/predict.py
@@ -5,9 +5,13 @@ import torch
 from argus import load_model
 import os
 import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from PIL import Image
 
 from src.datatools.utils import resolve_tuple
+from src.datatools.transforms import resize_image_torch
 from src.datatools.read_write_up import ReadWriteUp
+from src.metrics.ssim import SSIM_v0 as ssim
 
 
 CONFIG_PATH = '/workdir/configs/predict_configs.yaml'
@@ -26,8 +30,10 @@ def predict(cfg: DictConfig) -> None:
         file_path=cfg.predict.data[0],
         dtype=np.uint16)
 
-    patterns = patterns[0].astype(np.float64)
-    patterns = torch.from_numpy(patterns).to(DEVICE)
+    patterns = patterns[0][0]
+    im_size = cfg.data.data_params.img_size
+    patterns_down = resize_image_torch(patterns, im_size)
+    patterns_orig = resize_image_torch(patterns, tuple(x*4 for x in im_size))
 
     # Load the model
     model = hydra.utils.instantiate(cfg.model)
@@ -37,12 +43,12 @@ def predict(cfg: DictConfig) -> None:
         cfg.predict.model.file_path[0], device=DEVICE,
         optimizer=None)
 
-    patterns = patterns[np.newaxis, ...].to(DEVICE)
+    patterns = patterns_down[np.newaxis, ...].to(DEVICE)
     pred = model.predict(patterns.float())
 
     fig, ax = plt.subplots(1, 2)
-    print('Patterns shape: ', patterns.shape)
-    print('Pred shape: ', pred.shape)
+    ssim_value = ssim()(patterns_orig[np.newaxis, ...], pred)
+    print('ssim value for prediction: ', ssim_value)
     ax[0].imshow(patterns[0][0].cpu().numpy(), cmap='gray')
     ax[1].imshow(pred[0][0].cpu().numpy(), cmap='gray')
     plt.savefig(cfg.predict.save_path[0])
